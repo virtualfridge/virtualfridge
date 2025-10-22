59a84aa35eb7527f9d465fbb285106e1590222bb
Rafe Murray <rafemurr@gmail.com>
2025-10-22 14:38:35 -0700

    Fix barcode route

--- backend/src/routes/barcode.ts
+++ backend/src/routes/barcode.ts
@@ -49,61 +49,55 @@ router.post('/', authenticateToken, async (req, res) => {
 
       // Extract English-only product data
       const foodTypeData: Partial<FoodType> = {
-        name: product.product_name_en || null,
-        brand: product.brands || null,
-        // quantity: product.quantity || null,
-        // ingredients: product.ingredients_text_en || null,
-        image: product.image_url || null,
-        shelfLifeDays: shelfLifeDays,
-        allergens:
-          product.allergens_hierarchy
-            ?.filter((a: string) => a.startsWith('en:'))
-            .map((a: string) => a.replace(/^en:/, '')) || null,
-        nutrients: {
-          calories: product.nutrients?.['energy-kcal_100g'] || null,
-          energyKj: product.nutrients?.['energy-kj_100g'] || null,
-          protein: product.nutrients?.proteins_100g || null,
-          fat: product.nutrients?.fat_100g || null,
-          saturatedFat: product.nutrients?.['saturated-fat_100g'] || null,
-          monounsaturatedFat:
-            product.nutrients?.['monounsaturated-fat_100g'] || null,
-          polyunsaturatedFat:
-            product.nutrients?.['polyunsaturated-fat_100g'] || null,
-          transFat: product.nutrients?.['trans-fat_100g'] || null,
-          cholesterol: product.nutrients?.cholesterol_100g || null,
-          carbohydrates: product.nutrients?.carbohydrates_100g || null,
-          sugars: product.nutrients?.['sugars_100g'] || null,
-          fiber: product.nutrients?.['fiber_100g'] || null,
-          salt: product.nutrients?.['salt_100g'] || null,
-          sodium: product.nutrients?.['sodium_100g'] || null,
-          calcium: product.nutrients?.['calcium_100g'] || null,
-          iron: product.nutrients?.['iron_100g'] || null,
-          potassium: product.nutrients?.['potassium_100g'] || null,
-        },
-        // other: {
+          name: product.product_name_en || null,
+          brand: product.brands || null,
+          // quantity: product.quantity || null,
+          // ingredients: product.ingredients_text_en || null,
+          image: product.image_url || null,
+          shelfLifeDays: shelfLifeDays,
+          allergens:
+            product.allergens_hierarchy
+              ?.filter((a: string) => a.startsWith('en:'))
+              .map((a: string) => a.replace(/^en:/, '')) || null,
+          nutrients: {
+            calories: product.nutrients?.['energy-kcal_100g'] || null,
+            energyKj: product.nutrients?.['energy-kj_100g'] || null,
+            protein: product.nutrients?.proteins_100g || null,
+            fat: product.nutrients?.fat_100g || null,
+            saturatedFat: product.nutrients?.['saturated-fat_100g'] || null,
+            monounsaturatedFat:
+              product.nutrients?.['monounsaturated-fat_100g'] || null,
+            polyunsaturatedFat:
+              product.nutrients?.['polyunsaturated-fat_100g'] || null,
+            transFat: product.nutrients?.['trans-fat_100g'] || null,
+            cholesterol: product.nutrients?.cholesterol_100g || null,
+            carbohydrates: product.nutrients?.carbohydrates_100g || null,
+            sugars: product.nutrients?.['sugars_100g'] || null,
+            fiber: product.nutrients?.['fiber_100g'] || null,
+            salt: product.nutrients?.['salt_100g'] || null,
+            sodium: product.nutrients?.['sodium_100g'] || null,
+            calcium: product.nutrients?.['calcium_100g'] || null,
+            iron: product.nutrients?.['iron_100g'] || null,
+            potassium: product.nutrients?.['potassium_100g'] || null,
+          },
+          // other: {
           // alcohol: product.nutriments?.['alcohol_100g'] || null,
-          caffeine: product.nutriments?.['caffeine_100g'] || null,
-        //   water: product.nutriments?.['water_100g'] || null,
-        // }
-      },
-
-      category_properties: {
-        ciqual_food_name: product.category_properties?.['ciqual_food_name:en'] || null,
-      },
-    };
-
-    foodType = await foodTypeModel.create(productData);
-  
-
-      // Does not already exist so we save it
-      foodType = await foodTypeModel.create(foodTypeData);
+          // caffeine: product.nutriments?.['caffeine_100g'] || null,
+          //   water: product.nutriments?.['water_100g'] || null,
+          // }
+        },
+        // category_properties: {
+        //   ciqual_food_name: product.category_properties?.['ciqual_food_name:en'] || null,
+        // },
+        // Does not already exist so we save it
+        foodType = await foodTypeModel.create(foodTypeData);
     }
 
-    const expirationDate = addDaysToDate(new Date(), foodType.shelfLifeDays);
+    const expirationDate = addDaysToDate(new Date(), foodType!.shelfLifeDays);
 
     const foodItem = await foodItemModel.create({
       userId: req.user!._id,
-      typeId: foodType._id,
+      typeId: foodType!._id,
       expirationDate: expirationDate,
       percentLeft: 100,
     });
@@ -117,6 +111,5 @@ router.post('/', authenticateToken, async (req, res) => {
     return res.status(500).json({ message: 'Internal server error' });
   }
 });
-
 
 export default router;
