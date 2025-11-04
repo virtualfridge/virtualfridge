1ee16b1d73e73f280dddba77c1c8e80dcd2a3dfe
itsdombo <dominicrou1@hotmail.com>
2025-11-04 00:55:03 -0800

    added gradle build dependencies

--- frontend/macrobenchmark/build.gradle.kts
+++ frontend/macrobenchmark/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.test)
     alias(libs.plugins.kotlin.android)
@@ -17,6 +19,12 @@ android {
             "androidx.benchmark.suppressErrors" to "EMULATOR,LOW-BATTERY")
     }
 
+    // Align Java compatibility with app module (Java 11)
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
     buildTypes {
         // This benchmark buildType is used for benchmarking, and should function like your
         // release build (for example, with minification on). It"s signed with a debug key
@@ -30,6 +38,13 @@ android {
 
     targetProjectPath = ":app"
     experimentalProperties["android.experimental.self-instrumenting"] = true
+}
+
+// Align Kotlin JVM target with Java 11
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_11)
+    }
 }
 
 dependencies {
