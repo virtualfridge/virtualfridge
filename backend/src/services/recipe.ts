0d774988cb38f34bf9db38b8139187a32b5c2fa5
Rafe Murray <rafemurr@gmail.com>
2025-11-21 17:13:24 -0800

    Return only the recipe with best score based on nutritionn facts

--- backend/src/services/recipe.ts
+++ backend/src/services/recipe.ts
@@ -9,6 +9,8 @@ import {
   RecipeApiResponse,
   Recipe,
 } from '../types/recipe';
+import { foodTypeModel } from '../models/foodType';
+import { IFoodType, INutrients } from '../types/foodType';
 
 const DEFAULT_API_BASE_URL = 'https://www.themealdb.com/api/json/v1/1';
 const FILTER_ENDPOINT = '/filter.php';
@@ -53,7 +55,111 @@ export class RecipeService {
       }
     });
     await Promise.all(recipePromises);
-    return recipes;
+    // Now we score the recipes based on nutrition facts
+    // What we have:
+    // A list of ingredients
+    // A list of their measurements as strings :(
+    // The nutrition facts for the foods that are in the database
+    const badNutrients: Array<keyof INutrients> = [
+      'fat',
+      'saturatedFat',
+      'transFat',
+      'sugars',
+    ];
+    const goodNutrients: Array<keyof INutrients> = [
+      'fiber',
+      'calcium',
+      'iron',
+      'magnesium',
+      'zinc',
+      'potassium',
+    ];
+    var scorePromises = recipes.map(async recipe => {
+      const ingredients = [
+        recipe.strIngredient1,
+        recipe.strIngredient2,
+        recipe.strIngredient2,
+        recipe.strIngredient3,
+        recipe.strIngredient4,
+        recipe.strIngredient5,
+        recipe.strIngredient6,
+        recipe.strIngredient7,
+        recipe.strIngredient8,
+        recipe.strIngredient9,
+        recipe.strIngredient10,
+        recipe.strIngredient11,
+        recipe.strIngredient12,
+        recipe.strIngredient13,
+        recipe.strIngredient14,
+        recipe.strIngredient15,
+        recipe.strIngredient16,
+        recipe.strIngredient17,
+        recipe.strIngredient18,
+        recipe.strIngredient19,
+        recipe.strIngredient20,
+      ];
+      const foodTypePromises = ingredients.map(
+        async ingredient => await foodTypeModel.findByName(ingredient)
+      );
+
+      const foodTypes = await Promise.all(foodTypePromises);
+
+      return foodTypes
+        .filter(foodType => foodType !== null)
+        .reduce((currentScore: number, foodType) => {
+          var newScore = currentScore;
+          if (!foodType) {
+            return currentScore;
+          }
+          for (const nutrient of goodNutrients) {
+            newScore += this.getPercentDailyValue(nutrient, foodType);
+          }
+          for (const nutrient of badNutrients) {
+            newScore -= this.getPercentDailyValue(nutrient, foodType);
+          }
+          return newScore;
+        }, 0);
+    });
+    const scores = await Promise.all(scorePromises);
+    const maxScore = Math.max(...scores);
+    const bestIndex = scores.indexOf(maxScore);
+    const bestRecipe = recipes[bestIndex];
+    return [bestRecipe];
+  }
+
+  private getPercentDailyValue(
+    nutrient: keyof INutrients,
+    foodType: IFoodType
+  ) {
+    const dailyValues: Map<keyof INutrients, number> = new Map([
+      ['fat', 75],
+      ['saturatedFat', 10],
+      ['transFat', 10],
+      ['fiber', 28],
+      ['sugars', 100],
+      ['cholesterol', 0.3],
+      ['sodium', 2.3],
+      ['potassium', 3.4],
+      ['calcium', 1.3],
+      ['iron', 0.018],
+      ['magnesium', 0.023],
+      ['zinc', 0.011],
+    ]);
+
+    if (!foodType || !foodType.nutrients) {
+      return 0;
+    }
+    const amount = foodType.nutrients[nutrient];
+    if (!amount) {
+      return 0;
+    }
+    // For now assume all units in g
+    const amountGrams = parseInt(amount);
+    const dailyValue = dailyValues.get(nutrient);
+    if (!dailyValue) {
+      throw new Error('Nutrient not found in Daily Value table');
+    }
+    return amountGrams / dailyValue;
   }
 
   private async queryRecipesByIngredient(
